3dc6da571ba4db117a9b594aec3d6724a33cbdcf
kodurivamshi <vkoduri555@gmail.com>
2020-01-02 11:34:54 +0530

    first checkin

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express()
 const port = process.env.PORT || 3000
 
 //serve static files
-app.use(express.static(__dirname+'/dist/ngproject'));
+app.use(express.static(__dirname+'/dist/ngprojec'));
 
 //send index.html from home(root) route
-app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/dist/ngproject/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/dist/ngprojec/index.html')))
 
 app.listen(port, () => console.log('Example app listening on port ${port}!'))
